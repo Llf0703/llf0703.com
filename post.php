99cdbf8d1b6219f57629e87db49b0520096fef8c
Llf0703 <1667334662@qq.com>
2019-07-30 15:20:57 +0800

    query difficulty-tag from db; fix the bug of post.php

--- post.php
+++ post.php
@@ -15,6 +15,7 @@ $this->need('nav.php');
                     <i class="mdui-icon material-icons index-icon">&#xe192;</i> <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time> &nbsp;&nbsp;
                     <i class="mdui-icon material-icons index-icon">&#xe866;</i> <?php $this->category('  '); ?> &nbsp;&nbsp;
                     <a href="<?php $this->permalink() ?>#comments"><i class="mdui-icon material-icons index-icon">&#xe0b9;</i> <?php $this->commentsNum('%d 条评论'); ?></a>
+                    &nbsp;&nbsp; <?php if ($this->category=='sol'):?><?php $this->need('luogu.php')?><?php endif;?>
                 </div>
             </div>
         </div>
